e25153111f9e733e5a6df256f310229f9c197c8e
yaokuku123 <yaoqijun@outlook.com>
2023-02-11 18:22:46 +0800

    hadoop mr wordcount程序增加注释

--- hadoop_base/src/main/java/com/yqj/myhadoop/mapreduce/wc/MyWordCount.java
+++ hadoop_base/src/main/java/com/yqj/myhadoop/mapreduce/wc/MyWordCount.java
@@ -11,24 +11,35 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
+// 使用Maven打jar包，上传至服务器，手动使用linux命令，运行MR程序
+// hadoop jar hadoop_base-1.0-SNAPSHOT.jar com.yqj.myhadoop.mapreduce.wc.MyWordCount
 public class MyWordCount {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        // 加载配置文件
         Configuration conf = new Configuration();
+        // 使用配置文件参数创建job对象
         Job job = Job.getInstance(conf);
+        // 找到jar的start位置
         job.setJarByClass(MyWordCount.class);
         job.setJobName("my_word_count");
+        // 设置输入文件路径
         Path input = new Path("/data/wc/input");
         TextInputFormat.addInputPath(job,input);
+        // 设置输出文件路径
         Path output = new Path("/data/wc/output");
+        // 如果存在输出文件目录则删除
         if (output.getFileSystem(conf).exists(output)) {
             output.getFileSystem(conf).delete(output,true);
         }
         TextOutputFormat.setOutputPath(job,output);
-
+        // 设置mapper
         job.setMapperClass(MyMapper.class);
-        job.setReducerClass(MyReduce.class);
+        // 设置mapper的输出k-v类型
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
+        // 设置reducer
+        job.setReducerClass(MyReduce.class);
+        // 启动job
         job.waitForCompletion(true);
     }
 }
